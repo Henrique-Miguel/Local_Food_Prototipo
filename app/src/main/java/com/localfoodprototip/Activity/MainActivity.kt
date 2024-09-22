1010698fbfe1bb4528c3b01d159a74e8c2f2e106
Yara Setoue <ysetoue@gmail.com>
2024-09-22 17:21:09 -0300

    Layout de login e inicio

---
+++ app/src/main/java/com/localfoodprototip/Activity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.localfoodprototip.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.localfoodprototip.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnEntrar.setOnClickListener{
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }
+    }
+}
